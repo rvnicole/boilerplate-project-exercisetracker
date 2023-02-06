ffbf295687f65ffad18d7e4024c1df7bbe53928f
Nicole <106133672+rvnicole@users.noreply.github.com>
2023-02-06 20:30:06 +0000

    Agregada la Variable de Entorno con la URI

--- index.js
+++ index.js
@@ -16,4 +16,5 @@ const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
+const mySecret = process.env['URI_BD'];
 
